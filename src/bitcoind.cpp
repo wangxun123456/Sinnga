fef72bb99ccc979c1fb1d6b2c73a7ec29c431968
Zhu Zhenlong <zzl@palliums.org>
2018-09-20 09:51:05 +0800

    添加注释，测试github使用

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -185,7 +185,7 @@ int main(int argc, char* argv[])
 {
     SetupEnvironment();
 
-    // Connect bitcoind signal handlers
+    // Connect bitcoind signal handlers 
     noui_connect();
 
     return (AppInit(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE);
