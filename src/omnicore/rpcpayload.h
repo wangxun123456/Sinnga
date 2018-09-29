584a9af1abac16c5594341893eb67abf9cd5905a
Zhaofu Zhang <zhang_zhao_fu@sina.com>
2018-09-29 13:51:01 +0800

    Change param to JSONRPCRequest

--- src/omnicore/rpcpayload.h
+++ src/omnicore/rpcpayload.h
@@ -3,21 +3,23 @@
 
 #include <univalue.h>
 
-UniValue omni_createpayload_simplesend(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_sendall(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_dexsell(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_dexaccept(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_sto(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_issuancefixed(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_issuancecrowdsale(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_issuancemanaged(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_closecrowdsale(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_grant(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_revoke(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_changeissuer(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_trade(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_canceltradesbyprice(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_canceltradesbypair(const UniValue& params, bool fHelp);
-UniValue omni_createpayload_cancelalltrades(const UniValue& params, bool fHelp);
+class JSONRPCRequest;
+
+UniValue omni_createpayload_simplesend(const JSONRPCRequest &request);
+UniValue omni_createpayload_sendall(const JSONRPCRequest &request);
+UniValue omni_createpayload_dexsell(const JSONRPCRequest &request);
+UniValue omni_createpayload_dexaccept(const JSONRPCRequest &request);
+UniValue omni_createpayload_sto(const JSONRPCRequest &request);
+UniValue omni_createpayload_issuancefixed(const JSONRPCRequest &request);
+UniValue omni_createpayload_issuancecrowdsale(const JSONRPCRequest &request);
+UniValue omni_createpayload_issuancemanaged(const JSONRPCRequest &request);
+UniValue omni_createpayload_closecrowdsale(const JSONRPCRequest &request);
+UniValue omni_createpayload_grant(const JSONRPCRequest &request);
+UniValue omni_createpayload_revoke(const JSONRPCRequest &request);
+UniValue omni_createpayload_changeissuer(const JSONRPCRequest &request);
+UniValue omni_createpayload_trade(const JSONRPCRequest &request);
+UniValue omni_createpayload_canceltradesbyprice(const JSONRPCRequest &request);
+UniValue omni_createpayload_canceltradesbypair(const JSONRPCRequest &request);
+UniValue omni_createpayload_cancelalltrades(const JSONRPCRequest &request);
 
 #endif // OMNICORE_RPCPAYLOAD_H
